756ba993068ef82b69c7376eb0d03d2e86f2371a
Andre Harper <harperandre277@gmail.com>
2025-02-02 01:05:23 -0500

    added header and footer

--- apps/frontend/creator-frontend/src/App.tsx
+++ apps/frontend/creator-frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import Header from "./components/header/Header"
+import Header from '../../shared/components/Header'
+import Footer from '../../shared/components/Footer'
+import HomePage from '../../shared/Pages/HomePage'
 
+const App = () => {
+        
 
-export default function App() {
-    return (
-       <>
-       <Header />
-       </>
-    )
-}
+        return (
+         
+            
+              <div className="flex flex-col min-h-screen">
+                <Header />
+                <main className="flex-grow">
+                  <HomePage />
+                </main>
+                <Footer />
+              </div>
+  
+              
+             
+          
+        );
+      }
+    
+export default App;
