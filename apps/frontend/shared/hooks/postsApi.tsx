57adc0b8e6b38efc97e569e2e0f92323e39366df
itsdrelolz <harperandre277@gmail.com>
2025-02-13 13:12:28 -0500

    removed frontend directory

--- apps/frontend/shared/hooks/postsApi.tsx
+++
@@ -1,9 +0,0 @@
-
-export const fetchPosts = async () => { 
-    const response = await fetch('http://localhost:3000/home');
-    if (!response.ok) { 
-        throw new Error('Failed to fetch posts')
-        
-    }
-    return response.json();
-}
